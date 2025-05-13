752a5575d797d60ea7cf131611142bbc3d9cc5c1
Lazar <markoviclaki03.4@gmail.com>
2025-05-13 20:01:33 +0200

    just pushing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const qwertyKeys = [
     "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
-     "A", "S", "D", "F", "G", "H", "J", "K", "L",
+    "A", "S", "D", "F", "G", "H", "J", "K", "L",
     "↵", "Z", "X", "C", "V", "B", "N", "M", "⌫"
 ];
 
@@ -26,29 +26,50 @@ function createKeyboard() {
     let index = 0;
     let num = 10;
     let keyboard = document.getElementById('keyboard');
+
     for (let i = 0; i < 3; i++) {
         if (i == 1) { num--; }
         let keyColumn = document.createElement('div');
         keyColumn.classList.add('key-row');
-
         for (let j = 0; j < num; j++) {
-                let key = document.createElement('div');
-                key.classList.add('key');
-                let button = document.createElement('button');
-                button.classList.add('key-btn');
-                button.innerText = qwertyKeys[index++];
-                key.appendChild(button);
-                keyColumn.appendChild(key);
-            if ((i == 2 && j == 0) || (i == 2 && j == 8))
-            {
+            let key = document.createElement('div');
+            key.classList.add('key');
+            let button = document.createElement('button');
+            button.classList.add('key-btn');
+            button.innerText = qwertyKeys[index++];
+            
+            button.addEventListener("click", LetterClick);
+           
+            key.appendChild(button);
+
+
+            keyColumn.appendChild(key);
+            if ((i == 2 && j == 0) || (i == 2 && j == 8)) {
                 key.classList.add('bigger-key');
             }
         }
         keyboard.appendChild(keyColumn);
     }
+}
 
+function LetterClick(){
+    console.log(this.innerText);
+}
+
+function GameLogic() {
+    let start = document.querySelector('[data-i="${i}"][data-j="${j}"]');
+    console.log(start);
     
     
 }
-createTiles();
-createKeyboard();
+
+
+
+
+
+function Game() {
+    createTiles();
+    createKeyboard();
+}
+
+Game();
